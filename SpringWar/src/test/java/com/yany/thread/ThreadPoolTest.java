425148370134e0093baa8ae72c6194a943bd6d1c
yany8060 <806055720@qq.com>
2017-02-28 13:52:43 +0800

    ThreadPoolTest

---
+++ SpringWar/src/test/java/com/yany/thread/ThreadPoolTest.java
@@ -0,0 +1,112 @@
+package com.yany.thread;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.*;
+
+/**
+ * Created by yanyong on 2017/2/28.
+ */
+public class ThreadPoolTest {
+
+    /**
+     * 创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+     */
+//    @Test
+    public void cachedThreadPoolTest() {
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(index * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            cachedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Thread Name:" + Thread.currentThread().getName());
+                }
+            });
+        }
+
+
+    }
+
+    /**
+     * 创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待
+     */
+    @Test
+    public void fixedThreadPoolTest() {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            fixedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(2000);
+                        System.out.println("Thread Name:" + Thread.currentThread().getName());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 创建一个定长线程池，支持定时及周期性任务执行
+     */
+//    @Test
+    public void scheduledThreadPoolTest() {
+        ScheduledExecutorService scheduleThreadPool = Executors.newScheduledThreadPool(10);
+        scheduleThreadPool.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread Name:" + Thread.currentThread().getName());
+            }
+        }, 3, TimeUnit.SECONDS);
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行
+     */
+//    @Test
+    public void singleThreadPoolTest() {
+        ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            singleThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("Thread Name:" + Thread.currentThread().getName());
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
